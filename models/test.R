094051af9dc3bbf33cf57ce4be80f73768edae1b
fastscore <46939495+fastscore@users.noreply.github.com>
2019-02-14 13:17:48 -0600

    Add test

---
+++ models/test.R
@@ -0,0 +1,11 @@
+# fastscore.slot.0: in-use
+# fastscore.slot.1: in-use
+# fastscore.module-attached: gbm
+library(gbm)
+
+# Sample input: {"x":5.0, "y":6.0}
+action <- function(datum) {
+  x <- datum$x
+  y <- datum$y
+  emit(x + y)
+}
