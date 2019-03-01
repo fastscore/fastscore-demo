bcac9dbcf1fe9449db9fecbf92986d69ac7c06ab
fastscore <46939495+fastscore@users.noreply.github.com>
2019-03-01 08:10:39 -0700

    Add basel

---
+++ models/basel.c
@@ -0,0 +1,30 @@
+#include <fastscore.h>
+#include <assert.h>
+#include <stdio.h>
+#include <math.h>
+
+void begin(){}
+
+void action(fastscore_value_t v, int slot, int seqn)
+{
+	assert(v.fmt == FASTSCORE_FMT_JSON);
+
+	int n = json_integer_value(v.js);
+
+	float b = 0.0f;
+	for (int i = 0; i < n; i++)
+	{
+		b += 1 / (i * i);
+	}
+
+	b *= 6;
+	b = sqrt(b);
+	
+	fastscore_value_t o;
+	o.fmt = FASTSCORE_FMT_JSON;
+	o.json = json_integer(b);
+
+	fastscore_emit(o, 1);
+}
+
+void end1(){}
