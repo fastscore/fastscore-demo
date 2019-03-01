b37015d16aec7f448bb9a39a79ecd2b9b133d2cd
fastscore <46939495+fastscore@users.noreply.github.com>
2019-03-01 08:44:15 -0700

    Add basel

--- models/basel.c
+++ models/basel.c
@@ -12,15 +12,15 @@ void action(fastscore_value_t v, int slot, int seqn)
 	int n = json_integer_value(v.js);
 
 	float b = 0.0f;
-	/*
+	
 	for (int i = 0; i < n; i++)
 	{
 		b += 1.0f / (float)(i * i);
 	}
 
-	b *= 6.0f;
-	b = sqrt(b);
-	*/
+	// b *= 6.0f;
+	// b = sqrt(b);
+	
 	fastscore_value_t o;
 	o.fmt = FASTSCORE_FMT_JSON;
 	o.js = json_real(b);
